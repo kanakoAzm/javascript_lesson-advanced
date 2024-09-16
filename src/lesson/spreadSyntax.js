7d5e1d6e4bd7b82c808c22a955dc84cefbad910d
kanakoAzm <knka0717@gmail.com>
2024-09-16 16:12:16 +0900

    JS Lesson 発展編Section6 スプレッド構文の練習問題を解答

--- src/lesson/spreadSyntax.js
+++ src/lesson/spreadSyntax.js
@@ -1,11 +1,51 @@
 // ここにコードを書きながら確認しましょう！
+// const numbers = [1, 2, 3, 4, 5];
+
+// console.log(numbers);
+// console.log(...numbers);
+
+// const numbers = [1, 2, 3, 4, 5];
+
+// const copyNumbers = [...numbers];
+
+// console.log(copyNumbers);
+
+// const numbers = [1, 2, 3];
+
+// const newNumbers = [...numbers, 4, 5];
+
+// console.log(newNumbers);
 
 
+// const animal = ['cat', 'dog'];
 
+// const mergeArray = [...numbers, ...animal];
 
+// console.log(mergeArray);
 
+const company = {
+  name: 'Gizumo',
+  place: 'Shibuya',
+  established: '2015年5月15日',
+};
 
+// console.log(...company);
 
+// const copy = {...company};
+
+// console.log(copy);
+
+// const newCompany = {...company, established: '2015年5月15日'};
+
+// console.log(newCompany);
+
+// const updateProperty = {
+//   name: 'Front End',
+// };
+
+// const updateCompany = {...company, ...updateProperty};
+
+// console.log(updateCompany);
 
 
 
@@ -17,6 +57,7 @@ const packageManager = ['npm', 'yarn', 'pnpm'];
 
 function arrayFn(array) {
   const framework = ['Vue', 'React', 'Svelte'];
+  return  [...array, ...framework];
 }
 
 console.log(arrayFn(packageManager));
@@ -32,4 +73,6 @@ const objectB = {
   version: 3,
 };
 
-console.log();
+const updateObject = {...objectA, ...objectB, routingLibrary: 'Vue Router'};
+
+console.log(updateObject);
