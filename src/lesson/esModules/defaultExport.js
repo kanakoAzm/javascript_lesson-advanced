26e31866b741332da3fd69a6c2b98085d5ea87ae
kanakoAzm <knka0717@gmail.com>
2024-09-17 11:10:52 +0900

    JS Lesson 発展編Section9 ECMAScriptモジュールの練習問題を解答

--- src/lesson/esModules/defaultExport.js
+++ src/lesson/esModules/defaultExport.js
@@ -1,18 +1,18 @@
 // ここにコードを書きながら確認しましょう！
 
+// const fuga = 'fuga';
 
+// // export default fuga;
 
-
-
-
-
-
+// export default function testFn(text) {
+//   return `hello ${text}`;
+// }
 
 
 
 
 // 練習問題
 // 問題 2
-function defaultFn() {
+export default function defaultFn() {
   return 'default export';
 }
--- src/lesson/esModules/defaultImport.js
+++ src/lesson/esModules/defaultImport.js
@@ -1,15 +1,15 @@
 // ここにコードを書きながら確認しましょう！
 
+// import dummyFN from './defaultExport.js';
 
-
-
-
-
-
-
+// console.log(dummyFN('Gizumo'));
 
 
 
 
 // 練習問題
 // 問題 2
+
+import testFn from "./defaultExport.js";
+
+console.log(testFn());
